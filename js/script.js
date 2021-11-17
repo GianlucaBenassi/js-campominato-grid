4141ef26968bcedfc2dab9678e1263121c025a72
Gianluca Benassi <benox1199@gmail.com>
2021-11-17 14:59:02 +0100

    generate blocks function

--- js/script.js
+++ js/script.js
@@ -6,24 +6,28 @@ const btnPlay = document.getElementById('play');
 btnPlay.addEventListener('click', function(){
     
     const difficulty = parseInt(document.getElementById('difficulty').value);
-    let blockN
+    let blockN;
+    let diffClass;
 
     switch(difficulty) {
         case 1:
 
             blockN = 100;
+            diffClass = 'easy';
             break;
         
         case 2:
             blockN = 81;
+            diffClass = 'medium';
             break;
 
         case 3:
             blockN = 49;
+            diffClass = 'hard';
 
     }
 
-    console.log(blockN);
+    blockGenerator(diffClass, blockN);
 
 });
 
@@ -33,3 +37,20 @@ btnPlay.addEventListener('click', function(){
 
 
 // ***** functions *****
+
+// generate blocks
+const blockGenerator = (diff, num) => {
+
+    const boxContainer = document.querySelector('main .container');
+
+    // clear container befor add blocks
+    boxContainer.innerHTML = '';
+
+    for (let i = 1; i <= num; i++) {
+        const divBox = document.createElement('div');
+        divBox.classList.add('box', diff);
+        divBox.innerText = i;
+        boxContainer.appendChild(divBox);
+    }
+
+}
